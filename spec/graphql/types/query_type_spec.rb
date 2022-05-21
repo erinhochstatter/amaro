a0be8647ae46c25e44b350f8f7ec0dc402764a48
Erin Hochstatter <ehochstatter@gmail.com>
2022-05-21 16:04:36 -0500

    Add scoops to the query spec

--- spec/graphql/types/query_type_spec.rb
+++ spec/graphql/types/query_type_spec.rb
@@ -3,10 +3,11 @@ require "rails_helper"
 RSpec.describe Types::QueryType do
   subject { graphql_instance({}, {}) }
   let(:pours) { create_list(:pour, 3) }
+  let(:scoops) { create_list(:scoop, 3) }
 
   context "fields" do
     subject { described_class }
-    let(:fields) { %w[pour pours ingredientRecipe ingredientRecipes recipe recipes] }
+    let(:fields) { %w[pour pours ingredientRecipe ingredientRecipes recipe recipes scoop scoops] }
 
     it "has the proper fields" do
       expect(subject.fields.keys).to match_array(fields)
@@ -25,6 +26,20 @@ RSpec.describe Types::QueryType do
 
     it "all of the pours" do
       expect(subject.pours).to match_array(pours)
+    end
+  end
+
+  describe "#scoop" do
+  let(:scoop) { scoops.first }
+
+    it "is the scoop for the given ID" do
+      expect(subject.scoop(id: scoops.first.id)).to eq(scoops.first)
+    end
+  end
+
+  describe "#scoops" do
+    it "all of the scoops" do
+      expect(subject.scoops).to match_array(scoops)
     end
   end
 end
